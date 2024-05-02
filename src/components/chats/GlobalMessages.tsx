0432f335b587fc75c11d06004d03dbf1a5ec42f4
BrokenDetector <99690426+BrokenDetector@users.noreply.github.com>
2024-05-02 14:27:43 +0300

    Render message date.

--- src/components/chats/GlobalMessages.tsx
+++ src/components/chats/GlobalMessages.tsx
@@ -43,61 +43,70 @@ const GlobalMessages: FC<GlobalMessagesProps> = ({ initialMessages, sessionId, s
 				const isCurrentUser = message.senderId === sessionId;
 				const hasNextMessageFromSameUser = messages[index - 1]?.senderId === messages[index].senderId;
 				const shouldntRenderName = messages[index + 1]?.senderId === messages[index].senderId; // if messages from this user more that one, render name only on first message
+				const prevMessageTimestamp = messages[index + 1]
+					? format(messages[index + 1]?.timestamp!, "dd MMM yyyy")
+					: 0;
 
 				return (
-					<div
-						key={nanoid()}
-						className={cn("flex items-end", {
-							"justify-end": isCurrentUser,
-							"mb-2.5": !hasNextMessageFromSameUser,
-						})}
-					>
+					<div key={nanoid()}>
+						{format(message.timestamp!, "dd MMM yyyy") !== prevMessageTimestamp && (
+							<span className="flex justify-center items-center text-muted-foreground rounded-full text-sm p-2">
+								{format(message.timestamp!, "dd MMM yyyy")}
+							</span>
+						)}
 						<div
-							className={cn("flex flex-col text-base max-w-sm mx-2", {
-								"order-1 items-end": isCurrentUser,
-								"order-2 items-start": !isCurrentUser,
+							className={cn("flex items-end", {
+								"justify-end": isCurrentUser,
+								"mb-2.5": !hasNextMessageFromSameUser,
 							})}
 						>
-							<span
-								className={cn("px-3 py-2 rounded-lg flex flex-col", {
-									"bg-primary text-primary-foreground": isCurrentUser,
-									"bg-muted": !isCurrentUser,
-									"rounded-br-none": !hasNextMessageFromSameUser && isCurrentUser,
-									"rounded-bl-none": !hasNextMessageFromSameUser && !isCurrentUser,
+							<div
+								className={cn("flex flex-col text-base max-w-sm mx-2", {
+									"order-1 items-end": isCurrentUser,
+									"order-2 items-start": !isCurrentUser,
 								})}
 							>
-								{message.senderId !== sessionId && (
-									<p
-										className={cn("font-semibold", {
-											hidden: shouldntRenderName || isCurrentUser,
-										})}
-									>
-										{message.sender.name}
-									</p>
-								)}
-								<span className="inline-block ml-2">
-									{message.text}
-									<span className="ml-3  text-xs text-muted-foreground">
-										{format(message.timestamp!, "HH:mm")}
+								<span
+									className={cn("px-3 py-2 rounded-lg flex flex-col", {
+										"bg-primary text-primary-foreground": isCurrentUser,
+										"bg-muted": !isCurrentUser,
+										"rounded-br-none": !hasNextMessageFromSameUser && isCurrentUser,
+										"rounded-bl-none": !hasNextMessageFromSameUser && !isCurrentUser,
+									})}
+								>
+									{message.senderId !== sessionId && (
+										<p
+											className={cn("font-semibold", {
+												hidden: shouldntRenderName || isCurrentUser,
+											})}
+										>
+											{message.sender.name}
+										</p>
+									)}
+									<span className="inline-block ml-2">
+										{message.text}
+										<span className="ml-3  text-xs text-muted-foreground">
+											{format(message.timestamp!, "HH:mm")}
+										</span>
 									</span>
 								</span>
-							</span>
-						</div>
+							</div>
 
-						<div
-							className={cn("relative size-7", {
-								"order-2": isCurrentUser,
-								"order-1": !isCurrentUser,
-								invisible: hasNextMessageFromSameUser,
-							})}
-						>
-							<Image
-								fill
-								className="rounded-full"
-								alt="Profile picture"
-								src={isCurrentUser ? sessionImg : message.sender.image}
-								sizes="28"
-							/>
+							<div
+								className={cn("relative size-7", {
+									"order-2": isCurrentUser,
+									"order-1": !isCurrentUser,
+									invisible: hasNextMessageFromSameUser,
+								})}
+							>
+								<Image
+									fill
+									className="rounded-full"
+									alt="Profile picture"
+									src={isCurrentUser ? sessionImg : message.sender.image}
+									sizes="28"
+								/>
+							</div>
 						</div>
 					</div>
 				);
